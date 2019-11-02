7670b12220f0f173180877bae58589cde2644f74
atlee19 <atleegraham14@gmail.com>
2019-11-02 17:40:49 -0400

    Repower succesfully tested

--- main.cpp
+++ main.cpp
@@ -3,16 +3,22 @@ using namespace std;
 
 int RePower(int number, int exponent){
     if(exponent == 0)
-        return 0;
-    if(exponent == 1)
         return 1;
+    if(exponent == 1)
+        return number;
     
     return number * RePower(number, exponent - 1);
 }
 
 int main()
 {
+    int inputNumber, inputExpo;
 
+    cout << "ENTER AN INTEGER: " << endl;
+    cin >> inputNumber;
+    cout << "ENTER AN EXPONENT: " << endl;
+    cin >> inputExpo; 
+    cout << "RESULT: " << RePower(inputNumber, inputExpo) << endl;
 
 
     return 0; 
