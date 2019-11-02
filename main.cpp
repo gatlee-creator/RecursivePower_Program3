05d68ea6ae0390c4e31af266dd862b8c50bbe32f
atlee19 <atleegraham14@gmail.com>
2019-11-02 17:28:13 -0400

    implemented RePower fun

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int RePower(int number, int exponent){
+    if(exponent == 0)
+        return 0;
+    if(exponent == 1)
+        return 1;
+    
+    return number * RePower(number, exponent - 1);
+}
+
 int main()
 {
 
