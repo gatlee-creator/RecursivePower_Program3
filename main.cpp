9df064541e4e96152e06053924b30ea1e9998ec5
atlee19 <atleegraham14@gmail.com>
2019-11-03 13:07:21 -0500

    implemented & tested idiot proof checks

--- main.cpp
+++ main.cpp
@@ -17,12 +17,26 @@ int main()
 
     while (true)
     {
+        //here we ask and check the integer 
         cout << "ENTER AN INTEGER: " << endl;
         cin >> inputNumber;
+        if(inputNumber < 0){
+            cout << "error: integer must be non-negative" << endl;
+            continue;
+        }
+
+        //here we ask and check the exponent 
         cout << "ENTER AN EXPONENT: " << endl;
         cin >> inputExpo; 
+        if(inputExpo < 0){
+            cout << "error: exponent must be a non-negative" << endl;
+            continue;
+        }
+
+        //here we call Repower and display the result
         cout << "RESULT: " << RePower(inputNumber, inputExpo) << endl;
 
+        //here we will ask user if they want to continue 
         cout << "ENTER ANOTHER NUMBER?" << endl;
         cout << "enter Y or any other key to quit" << endl;
         cin >> inputChoice;
